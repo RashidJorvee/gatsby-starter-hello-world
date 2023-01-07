129c8d08a873c4fe5531d2d2ee5b042c9822c111
M Rashid Raza <m.rashid_raza@yahoo.com>
2023-01-07 21:29:39 +0530

    Added CSS style file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import * as React from "react"
+import style from '../style/styles.css'
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+  <div className="centerAlign">
+    <h1>Welcome to GatsbyJS example site!</h1>
+    <p>We are happy that you are exploring Gatsby and want to learn or use GatsbyJS for your site development.</p>
+  </div>
+  )
 }
