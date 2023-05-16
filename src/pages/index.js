6d56fb4c1d8c488a454af230fd084b3219673b05
Rashid Jorvee <m.rashid_raza@yahoo.com>
2023-05-16 11:33:14 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,10 @@ import style from '../style/styles.css'
 export default function Home() {
   return (
   <div className="centerAlign">
+    <script src="https://assets.adoberesources.net/loader.js?orgId=5278CEFB5717428A7F000101%40AdobeOrg&instanceId=psenablement&env=prod&geo=va7"></script>
     <h1>Welcome to GatsbyJS example site!</h1>
     <p>We are happy that you are exploring Gatsby and want to learn or use GatsbyJS for your site development.</p>
   </div>
+    
   )
 }
