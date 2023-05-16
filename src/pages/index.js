08328f66d1d04e1903ad71abdff9d2c3fa179331
Rashid Jorvee <m.rashid_raza@yahoo.com>
2023-05-16 15:48:54 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,27 +6,9 @@ export default function Home() {
   return (
   <div className="centerAlign">
     <script type="text/javascript">
-        (function() {
-        var didInit = false;
-            function initMunchkin() {
-                if(didInit === false) {
-                didInit = true;
-                Munchkin.init('132-PNL-231');
-                }
-            }
-            var s = document.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src = '//munchkin.marketo.net/munchkin.js';
-            s.onreadystatechange = function() {
-                if (this.readyState == 'complete' || this.readyState == 'loaded') {
-                initMunchkin();
-                }
-            };
-            s.onload = initMunchkin;
-            document.getElementsByTagName('head')[0].appendChild(s);
-        })();
+       document.write(unescape("%3Cscript src='//munchkin.marketo.net/munchkin.js' type='text/javascript'%3E%3C/script%3E"));
     </script>
+    <script>Munchkin.init('132-PNL-231', {"wsInfo":"j0hUd9jO"});</script>
     <script src="https://assets.adoberesources.net/loader.js?orgId=5278CEFB5717428A7F000101%40AdobeOrg&instanceId=psenablement&env=prod&geo=va7"></script>
     <h1>Welcome to GatsbyJS example site!</h1>
     <p>We are happy that you are exploring Gatsby and want to learn or use GatsbyJS for your site development.</p>
